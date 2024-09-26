1f5880255588093e880980a7e8e8e55af7ef1703
Ayush Uttarwar <111568550+Ayush086@users.noreply.github.com>
2024-09-27 01:00:56 +0530

    Update myString.h

--- myString.h
+++ myString.h
@@ -1,43 +1,117 @@
 /* contains declaration and libraries used */
 #ifndef MYSTRING_H  // if not defined then define it
+#ifndef MYSTRING_H
 #define MYSTRING_H
 
-#include <iostream> 
+#include <iostream>
+#include <vector>
 
-class myString {
-    private:
-        char* data; // pointer to first element of char array
-        int length;  // to store the length of myString
+class TrieNode {
+public:
+    TrieNode* children[26]; // Assuming lowercase alphabet letters
+    bool isEndOfWord;
 
-    public:
-        // default ctor
-        myString();
+    TrieNode();
+    ~TrieNode();
+};
 
-        // parametrized ctor
-        myString(const char* str);
+class Trie {
+private:
+    TrieNode* root;
+    void deleteTrieNode(TrieNode* node);
 
-        // copy ctor
-        myString(const myString& src);
+    // Helper function to convert a character to index
+    int charToIndex(char c);
 
-        // default dtor
-        ~myString();
+public:
+    Trie();
+    ~Trie();
 
-        int size() const;  // data modification isn't possible inside const functions
+    // Insert a word into the trie
+    void insert(const std::string &word);
 
-        bool empty() const; // empty string check
+    // Search for a word in the trie
+    bool search(const std::string &word);
 
-        const char* c_str() const; // for string printing
+    // Check if there is any word in the trie that starts with the given prefix
+    bool startsWith(const std::string &prefix);
+};
 
-        char operator[](int index); // for char at given index
+class MyString {
+private:
+    char *data;    // Pointer to store string data
+    size_t length; // Size of the string
 
-        int find(const myString& subStr) const; // to find a substring in string (if exists then index will be returned)
+public:
+    // Default constructor
+    MyString();
 
-        myString &operator+=(const myString& other); // string concatenation
+    // Parameterized constructor
+    MyString(const char *str);
 
-        void erase(int pos, int len); // erase substring from string
+    // Copy constructor
+    MyString(const MyString &other);
 
-// overloading "<<" (insertion operator) for o/p (operator overloading)
-        friend std::ostream& operator<<(std::ostream&os, const myString&str); // asking os to print str whenever "cout << str" is called.
+    // Destructor
+    ~MyString();
+
+    // Assignment operator
+    MyString &operator=(const MyString &other);
+
+    // Concatenation (+=) operator
+    MyString &operator+=(const MyString &other);
+
+    // Subscript ([]) operator
+    char &operator[](size_t index);
+
+    // Get size of the string
+    size_t size() const;
+
+    // Check if string is empty
+    bool empty() const;
+
+    // Return C-style string
+    const char *c_str() const;
+
+    // Append string
+    void append(const MyString &other);
+
+    // Find substring
+    int find(const MyString& substr) const;
+
+    // Erase substring
+    void erase(size_t pos, size_t len);
+
+    // Overload insertion operator for easy output
+    friend std::ostream &operator<<(std::ostream &os, const MyString &str);
+
+    // string reversing w/0 affecting original string
+    MyString reverse() const;
+
+    // insert substring at given position
+    void insert(size_t pos, const MyString &str);
+
+    // replace substring with given substring at all occurrences
+    void replace(const MyString &target, const MyString &replacement);
+
+    // word count
+    size_t word_count() const;
+
+    // check palindrome
+    bool check_palindrome() const;
+
+    // password encrypt
+    void password_encrypt(const char* key);
+
+    // decrypt password
+    void password_decrypt(const char* key);
+
+    // minimum number of edits (insertions, deletions, or substitutions) required to transform one string into another.
+    int levenshtein_distance(const MyString &other) const;
+};
+
+#endif // MYSTRING_H
+
 
     
 };
